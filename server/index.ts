5d4e653259067655c41d2222736f50ad380ab509
Rodrigo Droguett <andres.stahr@gmail.com>
2026-01-21 14:36:55 -0300

    Config: Add Procfile to force npm start, add startup logging

--- server/index.ts
+++ server/index.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
 });
